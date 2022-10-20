1d520f5be3151571146df8af4dce7c26b54e3f6c
CarlosNavSeg <carlos.raco@hotmail.com>
2022-10-20 13:53:54 +0200

    Commit.

--- Practica4/Anexo/Ejercicio1/script.js
+++ Practica4/Anexo/Ejercicio1/script.js
@@ -1,11 +1,8 @@
-window.onload = function() {
-    document.getElementById('formulario') = validar
-}
-function validar(evento) {
-    var event = evento
-    Array.from(document.getElementsByTagName).forEach(input => {
-        if(input.value = "") {
-            event.preventDefault()
-        }
+function validar() {
+    Array.from(document.getElementsByTagName('input')).forEach(input => {
+    if(input.value = "") {
+       return false;
+    }
     })
+    return true
 }
